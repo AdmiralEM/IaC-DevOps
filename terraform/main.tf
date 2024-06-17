ed1a21bb2dd8d6b339ca62fd9bf21ec60fbf25eb
AdmiralEM <43482577+AdmiralEM@users.noreply.github.com>
2024-06-17 00:13:42 -0400

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
