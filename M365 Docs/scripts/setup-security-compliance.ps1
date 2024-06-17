2042db05e69f7a555156cd77c53973f9af891e7a
AdmiralEM <43482577+AdmiralEM@users.noreply.github.com>
2024-06-17 00:24:19 -0400

    Create setup-security-compliance.ps1

---
+++ M365 Docs/scripts/setup-security-compliance.ps1
@@ -0,0 +1,4 @@
+# PowerShell script to automate Security and Compliance configuration
+
+# Example: Create a new DLP policy
+New-DlpCompliancePolicy -Name "DLP Policy" -ExchangeLocation All -OneDriveLocation All -SharePointLocation All
